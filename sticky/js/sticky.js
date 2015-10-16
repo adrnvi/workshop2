3ec45fa441e7867236173bf9efcf25bd856e5eb2
adrnvi <adrian.senecki@gmail.com>
2015-10-16 11:08:23 +0200

    changes to sticky menu functionality - height changes upon scroll

--- sticky/js/sticky.js
+++ sticky/js/sticky.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 
     var menu = $("#mainMenu");
     var lastPositionTop = 0;
+    var menuHeight = menu.height();
 
 
     //$(window).on("scroll", function() {});
@@ -21,10 +22,21 @@ $(document).ready(function() {
         if(menu.hasClass("sticky") === false && $(this).scrollTop() > menu.offset().top) {
             lastPositionTop = menu.offset().top;
             menu.addClass("sticky");
+
+            menu.animate({
+                height: (menuHeight/4).toString()+"px"
+            }, 500);
+
+            //menu.css("height", "15px")
         }
 
         if( menu.hasClass("sticky") && $(this).scrollTop() < lastPositionTop) {
             menu.removeClass("sticky");
+
+            menu.animate({
+                height: (menuHeight).toString()+"px"
+        }, 500);
+            //menu.removeAttr("style");
         }
 
 
